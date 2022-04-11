9ec2e2c2e8c5e76847839ff6b55fa07706892623
lunasaw <iszychen@gmail.com>
2022-04-11 15:13:19 +0800

    slot

---
+++ src/components/prop/ComponentI.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- todo-button 组件模板 -->
+  <el-button class="btn-primary">
+    <slot></slot>
+  </el-button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/prop/ComponentK.vue
@@ -0,0 +1,45 @@
+<template>
+  <slot :luna="luna" name="luna"></slot>
+  <ul>
+    <li v-for="( item, index ) in items" v-bind:key="index">
+      <!-- 添加一个图标 -->
+      <!-- <el-icon :size="15">
+        <avatar />
+      </el-icon>
+      <span class="green">{{ item.name }}</span> -->
+      <slot :item="item" :luna="luna" name="default"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { reactive, ref, toRefs } from '@vue/reactivity'
+
+export default {
+  setup () {
+    const items = reactive([
+      {
+        name: '张三',
+        age: 99
+      },
+      {
+        name: '李四',
+        age: 20
+      },
+      {
+        name: '王五',
+        age: 22
+      }
+    ])
+    const luna = ref('luna')
+    return {
+      items,
+      luna,
+      ...toRefs(items)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
